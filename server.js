2e86b01cda3e80ac190ec7dae69e525485f78139
sever <severin911@gmail.com>
2024-04-28 11:11:07 +0100

    Added public folder

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 // Import necessary modules
 const express = require('express');
 const path = require('path');
+const history = require('connect-history-api-fallback');
 
 // Create an Express app
 const app = express();
 
-// Serve static files from the 'public' directory (where your Vue app will be built)
-app.use(express.static(path.join(__dirname, 'public')));
+// Use 'connect-history-api-fallback' middleware which is necessary for SPA applications using HTML5 history mode
+app.use(history());
 
-// Set up a simple route for the homepage
+// Path to the static files
+const staticFileMiddleware = express.static(path.join(__dirname, 'front/dist'));
+
+// Serve static files from the 'front/dist' directory (where your Vue app will be built)
+app.use(staticFileMiddleware);
+
+// Set up a simple route for the homepage (this is actually handled by connect-history-api-fallback)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'front/dist', 'index.html'));
 });
 
 // Define the port for the server to listen on
